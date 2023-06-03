14aa13a9c16e67bb16c0200fa6a7f14f69dfacc6
Tomek <tomek7sa@gmail.com>
2023-06-03 07:09:27 +0200

    Create Sign in page and sign in component

---
+++ frontend/src/app/auth/signin/page.tsx
@@ -0,0 +1,12 @@
+import SigninForm from "../../../components/forms/SigninForm";
+
+const SigninPage = () => {
+  return (
+    <div className="flex flex-col w-full h-full justify-center items-center">
+    <h1>Sign in</h1>
+      <SigninForm />
+    </div>
+  );
+};
+
+export default SigninPage;
