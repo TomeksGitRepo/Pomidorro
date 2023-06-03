25317a99e8b603046bcbebaecae96e2d5aa4186c
Tomek <tomek7sa@gmail.com>
2023-06-03 07:10:00 +0200

    Change path

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,7 +4,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import ResponsiveAppBar from '../../components/navigation/ResponsiveAppbar';
+import ResponsiveAppBar from '../components/navigation/ResponsiveAppbar';
 
 const inter = Inter({ subsets: ['latin'] })
 
